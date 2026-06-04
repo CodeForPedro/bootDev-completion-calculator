44c3ee2cb0d2ca120de7ab5304bc6b485c58d6f7
CodeForPedro <codeforpedro@proton.me>
2026-06-04 10:48:29 -0400

    Add course data files

---
+++ devops-courses.js
@@ -0,0 +1,107 @@
+const learnToCodeInPython = {
+  name: "Learn to Code in Python",
+  length: 191,
+  difficulty: 1,
+};
+
+const learnLinux = {
+  name: "Learn Linux",
+  length: 67,
+  difficulty: 1,
+};
+
+const buildABookbot = {
+  name: "Build a Bookbot",
+  length: 12,
+  difficulty: 1,
+};
+
+const learnGit = {
+  name: "Learn Git",
+  length: 75,
+  difficulty: 1,
+};
+
+const learnObjectOrientedProgramming = {
+  name: "Learn Object Oriented Programming",
+  length: 61,
+  difficulty: 1,
+};
+
+const buildAsteroids = {
+  name: "Build Asteroids",
+  length: 20,
+  difficulty: 1,
+};
+
+const personalProject1 = {
+  name: "Personal Project 1",
+  length: 4,
+  difficulty: 1,
+};
+
+const learnGo = {
+  name: "Learn Go",
+  length: 189,
+  difficulty: 1,
+};
+
+const learnHTTPClients = {
+  name: "Learn HTTP Clients",
+  length: 83,
+  difficulty: 1,
+};
+
+const learnSQL = {
+  name: "Learn SQL",
+  length: 126,
+  difficulty: 1,
+};
+
+const learnHTTPServers = {
+  name: "Learn HTTP Servers",
+  length: 70,
+  difficulty: 1,
+};
+
+const learnDocker = {
+  name: "Learn Docker",
+  length: 43,
+  difficulty: 1,
+};
+
+const learnLoggingAndObservability = {
+  name: "Learn Logging and Observability",
+  length: 73,
+  difficulty: 1,
+};
+
+const learnAWS = {
+  name: "Learn AWS",
+  length: 93,
+  difficulty: 1,
+};
+
+const learnCICD = {
+  name: "Learn CI/CD",
+  length: 40,
+  difficulty: 1,
+};
+
+const learnKubernetes = {
+  name: "Learn Kubernetes",
+  length: 66,
+  difficulty: 1,
+};
+
+const capstoneProject = {
+  name: "Capstone Project",
+  length: 3,
+  difficulty: 1,
+};
+
+const learnHowToFindAProgrammingJob = {
+  name: "Learn How to Find a Programming Job",
+  length: 52,
+  difficulty: 1,
+};
---
+++ go-courses.js
@@ -0,0 +1,139 @@
+// Courses
+
+const learnToCodeInPython = {
+  name: "Learn to Code in Python",
+  length: 191,
+  difficulty: 1,
+};
+
+const learnLinux = {
+  name: "Learn Linux",
+  length: 67,
+  difficulty: 1,
+};
+
+const buildABookbot = {
+  name: "Build a Bookbot",
+  length: 12,
+  difficulty: 1,
+};
+
+const learnGit = {
+  name: "Learn Git",
+  length: 75,
+  difficulty: 1,
+};
+
+const learnObjectOrientedProgramming = {
+  name: "Learn Object Oriented Programming",
+  length: 61,
+  difficulty: 1,
+};
+
+const buildAsteroids = {
+  name: "Build Asteroids",
+  length: 20,
+  difficulty: 1,
+};
+
+const learnFunctionalProgramming = {
+  name: "Learn Functional Programming",
+  length: 89,
+  difficulty: 1,
+};
+
+const buildAnAIAgent = {
+  name: "Build an AI Agent",
+  length: 20,
+  difficulty: 1,
+};
+
+const learnDataStructuresAndAlgorithms = {
+  name: "Learn Data Structures and Algorithms",
+  length: 175,
+  difficulty: 1,
+};
+
+const buildAStaticSiteGenerator = {
+  name: "Build a Static Site Generator",
+  length: 28,
+  difficulty: 1,
+};
+
+const learnMemoryManagement = {
+  name: "Learn Memory Management",
+  length: 103,
+  difficulty: 1,
+};
+
+const personalProject1 = {
+  name: "Personal Project 1",
+  length: 4,
+  difficulty: 1,
+};
+
+const learnGo = {
+  name: "Learn Go",
+  length: 189,
+  difficulty: 1,
+};
+
+const learnHTTPClients = {
+  name: "Learn HTTP Clients",
+  length: 83,
+  difficulty: 1,
+};
+
+const buildAPokedex = {
+  name: "Build a Pokedex",
+  length: 12,
+  difficulty: 1,
+};
+
+const learnSQL = {
+  name: "Learn SQL",
+  length: 126,
+  difficulty: 1,
+};
+
+const buildABlogAggregator = {
+  name: "Build a Blog Aggregator",
+  length: 18,
+  difficulty: 1,
+};
+
+const learnHTTPServers = {
+  name: "Learn HTTP Servers",
+  length: 70,
+  difficulty: 1,
+};
+
+const learnFileServersAndCDNs = {
+  name: "Learn File Servers and CDNs",
+  length: 45,
+  difficulty: 1,
+};
+
+const learnDocker = {
+  name: "Learn Docker",
+  length: 43,
+  difficulty: 1,
+};
+
+const learnPubSubArchitecture = {
+  name: "Learn Pub/Sub Architecture",
+  length: 49,
+  difficulty: 1,
+};
+
+const capstoneProject = {
+  name: "Capstone Project",
+  length: 3,
+  difficulty: 1,
+};
+
+const learnHowToFindAProgrammingJob = {
+  name: "Learn How to Find a Programming Job",
+  length: 52,
+  difficulty: 1,
+};
